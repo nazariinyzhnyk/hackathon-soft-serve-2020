c1840806ed48bc208d0afd65d88cf5484020eaf9
Nazarii Nyzhnyk <nazar.nyzhnyk@gmail.com>
2020-05-17 00:39:42 +0300

    added thresholding to master

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import os
 
+from tqdm import tqdm
 from sklearn.model_selection import KFold
 from sklearn.metrics import fbeta_score
 from sklearn.preprocessing import MinMaxScaler
@@ -8,11 +9,13 @@ from category_encoders.basen import BaseNEncoder
 from imblearn.ensemble import BalancedRandomForestClassifier
 
 from utils import *
+from features import *
 
 RANDOM_SEED = 42
 set_seed(RANDOM_SEED)
 
 USE_SCALER = True
+USE_CACHED = False
 scaler = MinMaxScaler()
 
 cls = BalancedRandomForestClassifier
@@ -28,7 +31,6 @@ submission = pd.read_csv(os.path.join("data", "submission.csv"))
 df = history.merge(employees)
 labels = label_df(df)
 df['target'] = labels
-df = df[df.target != 2]
 
 columns_to_drop = ['EmployeeID', 'HiringDate', 'DismissalDate']
 
@@ -37,20 +39,40 @@ cat_columns = ['DevCenterID', 'SBUID', 'PositionID', 'PositionLevel',
                'IsInternalProject', 'OnSite', 'CompetenceGroupID', 'FunctionalOfficeID',
                'PaymentTypeId']
 
-X = df.drop(columns_to_drop, axis=1)
+num_features = ['Utilization', 'HourVacation', 'HourMobileReserve', 'HourLockedReserve', 'BonusOneTime', 'APM',
+                'WageGross']
+cat_features_num = ['DevCenterID', 'SBUID', 'PositionID', 'PositionLevel', 'IsTrainee', 'LanguageLevelID',
+                    'IsInternalProject', 'OnSite', 'CompetenceGroupID', 'FunctionalOfficeID', 'PaymentTypeId',
+                    'MonthOnPosition', 'MonthOnSalary']
 
-encoder = BaseNEncoder(cols=cat_columns, base = 2)
+if USE_CACHED:
+    df = pd.read_csv('df.csv')
+else:
+    for num_feature in tqdm(num_features):
+        df[num_feature + '_diff'], df[num_feature + '_lag'] = get_prev_values_stats(df, num_feature)
+        df[num_feature + '_nuniques'], df[num_feature + '_nunique_fracs'], df[
+            num_feature + '_time_since_last_change_vals'] = get_all_feat_values(df, num_feature)
+        df[num_feature + '_max'], df[num_feature + '_min'], df[num_feature + '_std'], df[num_feature + '_mean'], \
+            df[num_feature + '_lmmin'], df[num_feature + '_lmmax'], \
+            df[num_feature + '_lmmean'] = get_num_feat_values(df, num_feature)
+    df.to_csv('df.csv', index=False)
+
+    for cat_feature in tqdm(cat_features_num):
+        df[cat_feature + '_diff'], df[cat_feature + '_lag'] = get_prev_values_stats(df, cat_feature)
+        df[cat_feature + '_nuniques'], df[cat_feature + '_nunique_fracs'], df[
+            '_time_since_last_change_vals'] = get_all_feat_values(df, cat_feature)
+    df.to_csv('df.csv', index=False)
+
+df_x = df[df.target != 2]
+X = df_x.drop(columns_to_drop, axis=1)
+
+encoder = BaseNEncoder(cols=cat_columns, base=2)
 X = encoder.fit_transform(X)
 
 
 X["month"] = X["Date"].apply(get_month)
 X["year"] = X["Date"].apply(get_year)
 X = X.sort_values(["year", "month"])
-splits = [[[0,9],[10,11]],
-          [[0,11],[12,13]],
-          [[0,13],[14,15]],
-          [[0,15],[16,17]],
-          [[0,17],[18,19]]]
 
 fold = 0
 scores = []
@@ -85,10 +107,9 @@ print(f'MEAN OF SCOREs: {np.mean(scores)}')
 model = model_fit(X, y, classifier=cls, **cls_params)
 
 
-history.loc[:, 'Date'] = list(pd.to_datetime(history['Date']))
-df = history.merge(employees)
-df['target'] = labels
 X_test = df[df.target == 2]
+X_test.loc[:, 'Date'] = list(pd.to_datetime(X_test['Date']))
+
 X_test = X_test[X_test.Date == datetime(2019, 2, 1)]
 X_test = X_test[X_test.EmployeeID.isin(set(submission.EmployeeID))]
 emp_ids = X_test.EmployeeID
@@ -96,6 +117,6 @@ X_test = X_test.drop(columns_to_drop, axis=1)
 X_test = encoder.transform(X_test)
 X_test = X_test.drop(columns=["Date", 'target'])
 preds = model.predict(X_test)
-
+print(sum(preds))
 result = pd.DataFrame({'EmployeeID': emp_ids, 'target': preds})
 result.to_csv('submission.csv', index=False)
